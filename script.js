465b2106dd653a1f5f720c67492ad31f7d7707df
subhanksuyal <subhanksuyal20@gmail.com>
2024-12-16 11:24:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let yes =0;
+let no=0;
+function yesf(){
+    yes++;
+    document.getElementById("yes").innerHTML="yes: " +yes;
+}
+function nof(){
+    no++;
+    document.getElementById("no").innerHTML="no: " +no;
+}
